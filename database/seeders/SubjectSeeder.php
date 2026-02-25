29d80bb90c07713e73f98a5d1f16d6eac88279b4
maalakian04-cmd <maalakian04@gmail.com>
2026-02-25 20:40:45 +0800

    Old Cur

--- database/seeders/SubjectSeeder.php
+++ database/seeders/SubjectSeeder.php
@@ -512,23 +512,281 @@ class SubjectSeeder extends Seeder
                 'subject_name' => 'Student Internship Program 1 (600 hrs)',
                 'lec' => 6,
                 'lab' => 0,
-                'total' => 6,
+                'units' => 6,
                 'pre_req' => '4th Year Standing',
                 'year_level' => 4,
                 'semester' => 'First Semester'
             ],
-
+            // ===================== SECOND YEAR - OLD CURRICULUM =====================
+            // First Year - First Semester
+            [
+                'course_code' => 'CPT05',
+                'subject_name' => 'Computer Programming and Operating System ',
+                'lec' => 2,
+                'lab' => 3,
+                'units' => 5,
+                'pre_req' => 'CPT03',
+                'year_level' => 2,
+                'semester' => 'First Semester'
+            ],
+            [
+                'course_code' => 'GEC05',
+                'subject_name' => 'Mathematics in the Modern World',
+                'lec' => 3,
+                'lab' => 0,
+                'units' => 3,
+                'pre_req' => null,
+                'year_level' => 2,
+                'semester' => 'First Semester'
+            ],
+            [
+                'course_code' => 'GEC06',
+                'subject_name' => 'Purposive Communication',
+                'lec' => 3,
+                'lab' => 0,
+                'units' => 3,
+                'pre_req' => null,
+                'year_level' => 2,
+                'semester' => 'First Semester'
+            ],
+             [
+                'course_code' => 'GEC07',
+                'subject_name' => 'Art Appreciation ',
+                'lec' => 3,
+                'lab' => 0,
+                'units' => 3,
+                'pre_req' => null,
+                'year_level' => 2,
+                'semester' => 'First Semester'
+            ],
+            [
+                'course_code' => 'GEC13',
+                'subject_name' => 'Individual/Dual Game Sports',
+                'lec' => 2,
+                'lab' => 0,
+                'units' => 2,
+                'pre_req' => 'PE001',
+                'year_level' => 2,
+                'semester' => 'First Semester'
+            ],
+            // First Year - Second Semester
+            [
+                'course_code' => 'CPT07',
+                'subject_name' => 'Computer Servicing & Installation ',
+                'lec' => 2,
+                'lab' => 3,
+                'units' => 5,
+                'pre_req' => 'CPT01',
+                'year_level' => 2,
+                'semester' => 'Second Semester'
+            ],
+            [
+                'course_code' => 'CPT08',
+                'subject_name' => 'Peripheral Interface and Memory System',
+                'lec' => 3,
+                'lab' => 0,
+                'units' => 3,
+                'pre_req' => 'CPT02',
+                'year_level' => 2,
+                'semester' => 'Second Semester'
+            ],
+            [
+                'course_code' => 'GEC08',
+                'subject_name' => 'Science, Technology and Society',
+                'lec' => 3,
+                'lab' => 0,
+                'units' => 3,
+                'pre_req' => null,
+                'year_level' => 2,
+                'semester' => 'Second Semester'
+            ],
+            [
+                'course_code' => 'GEC09',
+                'subject_name' => 'Ethics',
+                'lec' => 3,
+                'lab' => 0,
+                'units' => 3,
+                'pre_req' => null,
+                'year_level' => 2,
+                'semester' => 'Second Semester'
+            ],
+            [
+                'course_code' => 'GEC10',
+                'subject_name' => 'Kontekstwalisadong Komunikasyon sa Filipino',
+                'lec' => 3,
+                'lab' => 0,
+                'units' => 3,
+                'pre_req' => null,
+                'year_level' => 2,
+                'semester' => 'Second Semester'
+            ],
+            [
+                'course_code' => 'GEC11',
+                'subject_name' => 'Filipno sa Ibat Ibang Disiplina',
+                'lec' => 3,
+                'lab' => 0,
+                'units' => 3,
+                'pre_req' => null,
+                'year_level' => 2,
+                'semester' => 'Second Semester'
+            ],
+            [
+                'course_code' => 'PE004',
+                'subject_name' => 'Team Sports/Games ',
+                'lec' => 2,
+                'lab' => 0,
+                'units' => 2,
+                'pre_req' => 'PE001',
+                'year_level' => 2,
+                'semester' => 'Second Semester'
+            ],
+            // ===================== THIRD YEAR - OLD CURRICULUM =====================
+            // Third Year - First Semester
+            [
+                'course_code' => 'CPT09',
+                'subject_name' => 'Microprocessor, Control System and Instrumentation',
+                'lec' => 2,
+                'lab' => 3,
+                'units' => 5,
+                'pre_req' => 'CPT03',
+                'year_level' => 3,
+                'semester' => 'First Semester'
+            ],
+             [
+                'course_code' => 'CPT10',
+                'subject_name' => 'Maintenance, Troubleshooting and Advanced Programming ',
+                'lec' => 2,
+                'lab' => 3,
+                'units' => 5,
+                'pre_req' => 'CPT03',
+                'year_level' => 3,
+                'semester' => 'First Semester'
+            ],
+            [
+                'course_code' => 'NLC01',
+                'subject_name' => 'New Labor Code',
+                'lec' => 3,
+                'lab' => 0,
+                'units' => 3,
+                'pre_req' => null,
+                'year_level' => 3,
+                'semester' => 'First Semester'
+            ],
+            [
+                'course_code' => 'SEM01',
+                'subject_name' => 'Seminar in Information Trends and Specialized Technology ',
+                'lec' => 3,
+                'lab' => 0,
+                'units' => 3,
+                'pre_req' => null,
+                'year_level' => 3,
+                'semester' => 'First Semester'
+            ],
+            [
+                'course_code' => 'HBO01',
+                'subject_name' => 'Human Behavior Organization',
+                'lec' => 3,
+                'lab' => 0,
+                'units' => 3,
+                'pre_req' => null,
+                'year_level' => 3,
+                'semester' => 'First Semester'
+            ],
+            [
+                'course_code' => 'ECM01',
+                'subject_name' => 'Entrepreneurial/Cooperative Management',
+                'lec' => 3,
+                'lab' => 0,
+                'units' => 3,
+                'pre_req' => null,
+                'year_level' => 3,
+                'semester' => 'First Semester'
+            ],
+            // Third Year - Second Semester
+            [
+                'course_code' => 'CPT11',
+                'subject_name' => 'Networking (LAN, WAN, and Internet)',
+                'lec' => 2,
+                'lab' => 3,
+                'units' => 5,
+                'pre_req' => 'CPT09,CPT10',
+                'year_level' => 3,
+                'semester' => 'Second Semester'
+            ],
+            [
+                'course_code' => 'RES01',
+                'subject_name' => 'Elements of Research and Project Study ',
+                'lec' => 3,
+                'lab' => 0,
+                'units' => 3,
+                'pre_req' => null,
+                'year_level' => 3,
+                'semester' => 'Second Semester'
+            ],
+            [
+                'course_code' => 'PER01',
+                'subject_name' => 'Personnel Administration',
+                'lec' => 3,
+                'lab' => 0,
+                'units' => 3,
+                'pre_req' => null,
+                'year_level' => 3,
+                'semester' => 'Second Semester'
+            ],
+            [
+                'course_code' => 'IPM01',
+                'subject_name' => 'Industrial Planning and Material Handling',
+                'lec' => 3,
+                'lab' => 0,
+                'units' => 3,
+                'pre_req' => null,
+                'year_level' => 3,
+                'semester' => 'Second Semester'
+            ],
+            [
+                'course_code' => 'IPM01',
+                'subject_name' => 'Industrial Planning and Material Handling',
+                'lec' => 3,
+                'lab' => 0,
+                'units' => 3,
+                'pre_req' => null,
+                'year_level' => 3,
+                'semester' => 'Second Semester'
+            ],
+            // ===================== FOURTH YEAR - OLD CURRICULUM =====================
+            // Fourth Year - First Semester
+            [
+                'course_code' => 'RES02',
+                'subject_name' => 'Research/Project Study and Feasibility Writing ',
+                'lec' => 3,
+                'lab' => 0,
+                'units' => 3,
+                'pre_req' => 'RES01',
+                'year_level' => 4,
+                'semester' => 'First Semester'
+            ],
+            [
+                'course_code' => 'OJT01',
+                'subject_name' => 'On-the-Job Training 1 (300 hours)',
+                'lec' => 9,
+                'lab' => 0,
+                'units' => 9,
+                'pre_req' => null,
+                'year_level' => 4,
+                'semester' => 'First Semester'
+            ],
             // Fourth Year - Second Semester
             [
-                'course_code' => 'SIP02',
-                'subject_name' => 'Student Internship Program 2 (600 hrs)',
-                'lec' => 6,
+                'course_code' => 'OJT02',
+                'subject_name' => 'On-the-Job Training 2 (700 hours)',
+                'lec' =>15,
                 'lab' => 0,
-                'total' => 6,
-                'pre_req' => 'SIP01',
+                'units' => 15,
+                'pre_req' => 'OJT01',
                 'year_level' => 4,
                 'semester' => 'Second Semester'
             ],
+
         ];
 
         // Insert all subjects
